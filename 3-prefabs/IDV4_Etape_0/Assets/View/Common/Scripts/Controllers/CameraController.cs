d8252b4b7f3362f0aea77e706869418c87f05351
ngayon_p <ngayon_p@etna-alternance.net>
2024-12-04 00:58:31 +0100

    step 3: Prefabs

---
+++ 3-prefabs/IDV4_Etape_0/Assets/View/Common/Scripts/Controllers/CameraController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform target;
+    public float lerpSpeed = 15f;
+
+    void LateUpdate()
+    {
+        if (target != null)
+        {
+            Vector3 newPosition = Vector3.Lerp(transform.position, target.position, lerpSpeed * Time.deltaTime);
+            transform.position = new Vector3(newPosition.x, newPosition.y, transform.position.z);
+        }
+    }
+}
